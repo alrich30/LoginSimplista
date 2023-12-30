913ac1aed0ca3c92d354d82f1f7c18ea0cbe7da4
alrich30 <richardtorresperes@gmail.com>
2023-12-30 08:43:09 -0400

    Primer commit subiendo proyecto a repositorio remoto

---
+++ js/formularyButton.js
@@ -0,0 +1,72 @@
+/*Este evento se encarga de enviar el formulario si se cumple una serie de parametros*/ 
+
+document.getElementById('miFormulario').addEventListener('submit', function (event) {
+    
+    let currentUserInput = document.querySelector('.user-input').value;
+    let currentPasswordInput = document.querySelector('.password-input').value;
+    let user = "capAmerica";
+    let password  = 123456;
+    let correctUser = false;
+    let validationErrorUser = document.querySelector('.validation-error-user');
+    let validationErrorPassword = document.querySelector('.validation-error-password');
+    
+    if(user === currentUserInput){
+        correctUser = true;
+        // Si el nombre de usuario es correcto se le da el valor de "true" a correctUser para ser usador despues
+    } else {
+        // Si el nombre de usuario es incorrecto una etiqueta <span> escondida aparecerá indicando que la contraseña 
+        // es incorrecta y no se enviara el formulario
+        validationErrorUser.style.transform = 'translateY(0px)';
+        validationErrorUser.style.opacity = '1'; 
+        event.preventDefault();
+
+    }
+
+    if (password != currentPasswordInput && correctUser === true){
+        // Si el nombre de usuario es correcto pero la contraseña no es correcta, una etiqueta escondida <span> 
+        // apareca desde el <input> indicando que ingrese la correcta.
+        validationErrorPassword.style.transform = 'translateY(0px)';
+        validationErrorPassword.style.opacity = '1';
+        event.preventDefault();
+    }
+});
+    
+    
+
+    
+
+
+
+
+
+/*function ocultarLabel() {
+    if (formularySend === true) {
+        const clickHandler = () => {
+            validationErrorUser.style.transform = 'translateY(-25px)';
+            validationErrorUser.style.opacity = '0';
+            validationErrorPassword.style.transform = 'translateY(-25px)';
+            validationErrorPassword.style.opacity = '0';
+            formularySend = false;
+    
+            // Remover el event listener después de ejecutar una vez
+            click.removeEventListener('click', clickHandler);
+        };
+    
+        click.addEventListener('click', clickHandler);
+    }
+}*/
+
+
+    
+
+
+
+
+
+
+
+
+
+    
+
+    
