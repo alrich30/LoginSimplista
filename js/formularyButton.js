633c5f3f4bb64a60994b5c9d52d37c52e0e7631e
Raian M. Gil del Rosario <rgil@lanzasoftware.com>
2023-12-30 12:32:45 -0400

    spaces fixes

--- js/formularyButton.js
+++ js/formularyButton.js
@@ -2,38 +2,38 @@
 
 // document.getElementById('miFormulario').addEventListener('submit', function (event) {
 document.getElementById('button').addEventListener('click', function (event) {
-  event.preventDefault();
-  let currentUserInput = document.querySelector('.user-input').value;
-  let currentPasswordInput = document.querySelector('.password-input').value;
-  console.log(currentUserInput, currentPasswordInput)
-  let user = "capAmerica";
-  let password = 123456;
-  let correctUser = false;
-  let validationErrorUser = document.querySelector('.validation-error-user');
-  let validationErrorPassword = document.querySelector('.validation-error-password');
+    event.preventDefault();
+    let currentUserInput = document.querySelector('.user-input').value;
+    let currentPasswordInput = document.querySelector('.password-input').value;
+    console.log(currentUserInput, currentPasswordInput)
+    let user = "capAmerica";
+    let password = 123456;
+    let correctUser = false;
+    let validationErrorUser = document.querySelector('.validation-error-user');
+    let validationErrorPassword = document.querySelector('.validation-error-password');
 
-  if (user === currentUserInput) {
-    correctUser = true;
-    validationErrorUser.style.transform = 'translateY(-25px)';
-    validationErrorUser.style.opacity = '0';
-    // Si el nombre de usuario es correcto se le da el valor de "true" a correctUser para ser usador despues
-  } else {
-    // Si el nombre de usuario es incorrecto una etiqueta <span> escondida aparecerá indicando que la contraseña 
-    // es incorrecta y no se enviara el formulario
-    validationErrorUser.style.transform = 'translateY(0px)';
-    validationErrorUser.style.opacity = '1';
-  }
+    if (user === currentUserInput) {
+      correctUser = true;
+      validationErrorUser.style.transform = 'translateY(-25px)';
+      validationErrorUser.style.opacity = '0';
+      // Si el nombre de usuario es correcto se le da el valor de "true" a correctUser para ser usador despues
+    } else {
+      // Si el nombre de usuario es incorrecto una etiqueta <span> escondida aparecerá indicando que la contraseña 
+      // es incorrecta y no se enviara el formulario
+      validationErrorUser.style.transform = 'translateY(0px)';
+      validationErrorUser.style.opacity = '1';
+    }
 
-  if (password != currentPasswordInput && correctUser === true) {
-    // Si el nombre de usuario es correcto pero la contraseña no es correcta, una etiqueta escondida <span> 
-    // apareca desde el <input> indicando que ingrese la correcta.
-    validationErrorPassword.style.transform = 'translateY(0px)';
-    validationErrorPassword.style.opacity = '1';
-  } else if (password == currentPasswordInput && correctUser == true) {
-    validationErrorPassword.style.transform = 'translateY(-25px)';
-    validationErrorPassword.style.opacity = '0';
-    alert('Bienvenido TurboBimbolo415!')
-  }
+    if (password != currentPasswordInput && correctUser === true) {
+      // Si el nombre de usuario es correcto pero la contraseña no es correcta, una etiqueta escondida <span> 
+      // apareca desde el <input> indicando que ingrese la correcta.
+      validationErrorPassword.style.transform = 'translateY(0px)';
+      validationErrorPassword.style.opacity = '1';
+    } else if (password == currentPasswordInput && correctUser == true) {
+      validationErrorPassword.style.transform = 'translateY(-25px)';
+      validationErrorPassword.style.opacity = '0';
+      alert('Bienvenido TurboBimbolo415!')
+    }
 });
 
 
